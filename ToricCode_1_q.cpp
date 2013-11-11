4e068dbf271d04ae1265a9fa3305747870ff36e5
lhayward <l3haywar@uwaterloo.ca>
2013-11-11 11:37:36 -0500

    Added code for plaqSpins_ array in ToricCode_1_q class

--- ToricCode_1_q.cpp
+++ ToricCode_1_q.cpp
@@ -29,9 +29,22 @@ ToricCode_1_q::ToricCode_1_q(std::ifstream* fin, std::string fileName,
         D_  = hcube_->getD();
         N0_ = hcube_->getN();
         N1_ = D_*N0_;
-        N2_ = (D_*(D_-1))/2*N0_;
+        N2_ = N0_*(D_*(D_-1))/2;  //Number of plaquettes is N0_ * (D choose 2)
+        plaqsPerSpin_ = 2*(D_-1);
         
         spins_ = new IsingSpins(alpha_, N1_);
+        
+        //create the plaqSpins_ and neighPlaqs_ arrays:
+        plaqSpins_ = new uint*[N2_];
+        for( uint i=0; i<N2_; i++ )
+        { plaqSpins_[i] = new uint[SPINS_PER_PLAQ_]; }
+        
+        neighPlaqs_ = new uint*[N1_];
+        for( uint i=0; i<N1_; i++ )
+        { neighPlaqs_[i] = new uint[plaqsPerSpin_]; }
+        
+        init_plaqArrays();
+        printPlaqs();
       }
       else
       {
@@ -58,13 +71,57 @@ ToricCode_1_q::ToricCode_1_q(std::ifstream* fin, std::string fileName,
 
 /******************************* ~ToricCode_1_q() (destructor) *******************************/
 ToricCode_1_q::~ToricCode_1_q()
-{ }
+{
+  if( spins_ != NULL )
+  { delete spins_; }
+  spins_ = NULL;
+  
+  //delete the plaqSpins_ array:
+  for(uint i=0; i<N2_; i++)
+  { 
+    if( plaqSpins_[i] != NULL )
+    { delete[] plaqSpins_[i]; }
+    plaqSpins_[i] = NULL; 
+  }
+  if( plaqSpins_ != NULL )
+  { delete[] plaqSpins_; }
+  plaqSpins_ = NULL;
+  
+  //delete the neighPlaqs_ array:
+  for(uint i=0; i<N1_; i++)
+  { 
+    if( neighPlaqs_[i] != NULL )
+    { delete[] neighPlaqs_[i]; }
+    neighPlaqs_[i] = NULL; 
+  }
+  if( neighPlaqs_ != NULL )
+  { delete[] neighPlaqs_; }
+  neighPlaqs_ = NULL;
+  
+}
 
 /************************************* init_plaqArrays() *************************************/
 void ToricCode_1_q::init_plaqArrays()
-{
+{ 
+  uint plaqNum=0; //counter for the plaquette number
+  
+  //loop to calculate the four 1-cells associated with each plaquette:
   for( uint zeroCell=0; zeroCell<N0_; zeroCell++ )
   {
+    //loop over the (D choose 2) plaquettes associated to each 0-cell:
+    for( uint i=0; i<D_; i++ )
+    {
+      for( uint j=(i+1); j<D_; j++ )
+      {
+        plaqSpins_[plaqNum][0] = D_*zeroCell + i;
+        plaqSpins_[plaqNum][1] = D_*zeroCell + j;
+        plaqSpins_[plaqNum][2] = D_*hcube_->getNeighbour(zeroCell,j) + i;
+        plaqSpins_[plaqNum][3] = D_*hcube_->getNeighbour(zeroCell,i) + j;
+        
+        plaqNum++;
+        //plaqSpins_
+      }
+    }
   } //closes loop over 0-cells
 }
 
@@ -81,7 +138,7 @@ void ToricCode_1_q::printParams()
 {
   if( isValid_ )
   {
-    std::cout << "(1,D-1) Toric Code Parameters:" << std::endl;
+    std::cout << "(1," << (D_-1) << ") Toric Code Parameters:" << std::endl;
     Model::printParams();
   }
   else
@@ -89,6 +146,21 @@ void ToricCode_1_q::printParams()
     std::cout << "ERROR in ToricCode_1_q::printParams(): the ToricCode_1_q object is not "
               << "valid\n" << std::endl;
   }
+}
+
+/**************************************** printPlaqs() ***************************************/
+void ToricCode_1_q::printPlaqs()
+{
+  std::cout << "Spins on Each Plaquette:" << std::endl;
+  
+  for( uint i=0; i<N2_; i++ )
+  {
+    std::cout << "  Plaquette " << i << ": [ ";
+    for( uint j=0; j<SPINS_PER_PLAQ_; j++)
+    { std::cout << plaqSpins_[i][j] << " "; }
+    std::cout << "]\n";
+  }
+  std::cout << std::endl;
 }
 
 /**************************************** printSpins() ***************************************/
--- ToricCode_1_q.h
+++ ToricCode_1_q.h
@@ -20,14 +20,17 @@ class ToricCode_1_q : public Model
     typedef unsigned int  uint;
     
   private:
+    static const uint  SPINS_PER_PLAQ_=4;
+    
     uint        D_;   //dimension
     uint        N0_;  //number of 0-cells (vertices)
     uint        N1_;  //number of 1-cells (bonds), i.e. the number of d.o.f. since the spins
                       //live on the bonds
     uint        N2_;  //number of 2-cells (plaquettes)
+    uint        plaqsPerSpin_;  //number of plaquettes touching each spin
     Hypercube*  hcube_; //the hypercubic lattice on which the d.o.f. live
     IsingSpins* spins_; //the degrees of freedom (d.o.f.) for the model
-    uint**      plaqSpins_; //plaqSpins_[i] stores the indices of the 4 spins touching 
+    uint**      plaqSpins_; //plaqSpins_[i] stores the indices of the spins touching
                             //plaquette i
     uint**      neighPlaqs_;  //neighPlaqs_[i] stores the indices of the 2(D-1) plaquettes
                               //touching spin i
@@ -41,6 +44,7 @@ class ToricCode_1_q : public Model
     
     virtual double calculateEnergy ();
     virtual void   printParams     ();
+    void           printPlaqs      ();
     virtual void   printSpins      ();
     virtual void   randomize       (MTRand* randomGen);
     virtual void   singleSpinUpdate();
